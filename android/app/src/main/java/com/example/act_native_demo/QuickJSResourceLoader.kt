ecc566029640365134189545a420271963357980
Luke Kaalim <lkaalim@nine.com.au>
2022-12-09 03:47:08 +1100

    Remove old test projects

--- android/app/src/main/java/com/example/act_native_demo/QuickJSResourceLoader.kt
+++
@@ -1,9 +0,0 @@
-package com.example.act_native_demo
-
-import com.whl.quickjs.wrapper.JSModule
-
-class QuickJSResourceLoader : JSModule.Loader {
-    override fun getModuleScript(moduleName: String?): String {
-        return "export const hello = \"World\""
-    }
-}
